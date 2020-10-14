eb3201dbe25480a3dc05a601282356799b2b2fca
Lucas Antonio Ramos Sartori <lucassartori@cookiecode.com.br>
2020-10-14 08:03:26 -0300

    update order items store

--- projects/admin/src/app/store/order-items/order-items.reducer.ts
+++ projects/admin/src/app/store/order-items/order-items.reducer.ts
@@ -1,21 +1,33 @@
-import { createEntityAdapter, EntityAdapter, EntityState } from '@ngrx/entity';
-import { OrderItem } from '../../core/models/order';
-import { Action, createReducer, on } from '@ngrx/store';
+import {createEntityAdapter, EntityAdapter, EntityState} from '@ngrx/entity';
+import {OrderItem} from '../../core/models/order';
+import {Action, createReducer, on} from '@ngrx/store';
 import {
-  ADD_ORDER_ITEM_DONE, CLEAR_OLD_DATA,
+  ADD_ORDER_ITEM,
+  ADD_ORDER_ITEM_DONE,
+  ADD_ORDER_ITEM_FAILED,
+  CLEAR_OLD_DATA,
+  DELETE_ORDER_ITEM,
   DELETE_ORDER_ITEM_DONE,
+  DELETE_ORDER_ITEM_FAILED,
+  REQUEST_ORDER_ITEMS,
   REQUEST_ORDER_ITEMS_DONE,
-  UPDATE_ORDER_ITEM_DONE
+  REQUEST_ORDER_ITEMS_FAILED,
+  UPDATE_ORDER_ITEM,
+  UPDATE_ORDER_ITEM_DONE,
+  UPDATE_ORDER_ITEM_FAILED
 } from './order-items.actions';
 
 export const featureKey = 'order-items';
 
 export interface OrderItemsState extends EntityState<OrderItem> {
+  loading: boolean;
 }
 
 const adapter: EntityAdapter<OrderItem> = createEntityAdapter<OrderItem>();
 
-const initialState: OrderItemsState = adapter.getInitialState({});
+const initialState: OrderItemsState = adapter.getInitialState({
+  loading: false
+});
 
 export const {
   selectAll,
@@ -23,14 +35,26 @@ export const {
 } = adapter.getSelectors();
 
 const orderItemReducer = createReducer(initialState,
+  on(REQUEST_ORDER_ITEMS,
+    UPDATE_ORDER_ITEM,
+    DELETE_ORDER_ITEM,
+    ADD_ORDER_ITEM,
+    (state) => ({...state, loading: true})
+  ),
+  on(REQUEST_ORDER_ITEMS_FAILED,
+    UPDATE_ORDER_ITEM_FAILED,
+    DELETE_ORDER_ITEM_FAILED,
+    ADD_ORDER_ITEM_FAILED,
+    (state) => ({...state, loading: false})
+  ),
   on(REQUEST_ORDER_ITEMS_DONE, (state, {items}) => {
     const newState = adapter.removeAll(state);
-    return adapter.addMany(items, newState);
+    return adapter.addMany(items, {...newState, loading: false});
   }),
-  on(UPDATE_ORDER_ITEM_DONE, (state, {item}) => adapter.upsertOne(item, state)),
-  on(DELETE_ORDER_ITEM_DONE, (state, {id}) => adapter.removeOne(id, state)),
-  on(ADD_ORDER_ITEM_DONE, (state, {item}) => adapter.addOne(item, state)),
-  on(CLEAR_OLD_DATA, (state) => adapter.removeAll(state))
+  on(UPDATE_ORDER_ITEM_DONE, (state, {item}) => adapter.upsertOne(item, {...state, loading: false})),
+  on(DELETE_ORDER_ITEM_DONE, (state, {id}) => adapter.removeOne(id, {...state, loading: false})),
+  on(ADD_ORDER_ITEM_DONE, (state, {item}) => adapter.addOne(item, {...state, loading: false})),
+  on(CLEAR_OLD_DATA, (state) => adapter.removeAll({...state, loading: false}))
 );
 
 export function reducer(state: OrderItemsState | undefined, action: Action) {
