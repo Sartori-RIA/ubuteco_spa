1f4206cd1133bcf9d806952379ddc1c6ce282bd9
Lucas Antonio Ramos Sartori <lucassartori@cookiecode.com.br>
2021-02-14 20:09:38 -0300

    prod url fix

--- projects/admin/src/environments/environment.prod.ts
+++ projects/admin/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  cable_url: 'ws://localhost:3000/api/cable',
-  api_url: 'http://localhost:3000/api/',
-  auth_url: 'http://localhost:3000/auth/',
+  cable_url: 'ws://ibuteco.herokuapp.com/api/cable',
+  api_url: 'https://ibuteco.herokuapp.com/api/',
+  auth_url: 'https://ibuteco.herokuapp.com/auth/',
 };
