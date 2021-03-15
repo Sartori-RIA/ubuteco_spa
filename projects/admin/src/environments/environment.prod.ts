57d5f9a2f3a9872aff20b2b24b939b3605b47b6e
Lucas Antonio Ramos Sartori <lucassartori@cookiecode.com.br>
2021-03-15 11:07:28 -0300

    changed ws to wss

--- projects/admin/src/environments/environment.prod.ts
+++ projects/admin/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  cable_url: 'ws://ibuteco.herokuapp.com/api/cable',
+  cable_url: 'wss://ibuteco.herokuapp.com/api/cable',
   api_url: 'https://ibuteco.herokuapp.com/api/',
   auth_url: 'https://ibuteco.herokuapp.com/auth/',
   bugsnag: '2f316a60d85fe821282d7a315fe977b5',
