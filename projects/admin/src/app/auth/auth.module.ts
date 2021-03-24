2bee6d70624c05261911184961323c2139bacb3b
Lucas Antonio Ramos Sartori <lucassartori@cookiecode.com.br>
2021-03-24 09:31:38 -0300

    travis

--- projects/admin/src/app/auth/auth.module.ts
+++ projects/admin/src/app/auth/auth.module.ts
@@ -7,12 +7,8 @@ import {PasswordUpdateComponent} from './password-update/password-update.compone
 import {EmailRecoverComponent} from './email-recover/email-recover.component';
 import {ReactiveFormsModule} from '@angular/forms';
 import {SharedModule} from '../shared/shared.module';
-import {StoreModule} from '@ngrx/store';
-import * as fromAuth from '../store/auth/auth.reducer';
-import {EffectsModule} from '@ngrx/effects';
-import {AuthEffects} from '../store/auth/auth.effects';
-import { AuthHomeComponent } from './auth-home.component';
-import { SignOutComponent } from './sign-out/sign-out.component';
+import {AuthHomeComponent} from './auth-home.component';
+import {SignOutComponent} from './sign-out/sign-out.component';
 import {MaterialModule} from '../material/material.module';
 
 @NgModule({
