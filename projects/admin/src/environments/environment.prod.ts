b750cbad3597b05e9ff068124388348242584ccd
Lucas Antonio Ramos Sartori <lucassartori@cookiecode.com.br>
2021-06-27 14:22:45 -0300

    added namespace to url

--- projects/admin/src/environments/environment.prod.ts
+++ projects/admin/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   cable_url: 'wss://ibuteco.herokuapp.com/api/cable',
-  api_url: 'https://ibuteco.herokuapp.com/api/',
+  api_url: 'https://ibuteco.herokuapp.com/api/v1/',
   auth_url: 'https://ibuteco.herokuapp.com/auth/',
   bugsnag: '2f316a60d85fe821282d7a315fe977b5',
 };
--- projects/admin/src/environments/environment.ts
+++ projects/admin/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   cable_url: 'ws://localhost:3000/api/cable',
-  api_url: 'http://localhost:3000/api/',
+  api_url: 'http://localhost:3000/api/v1/',
   auth_url: 'http://localhost:3000/auth/',
   bugsnag: '2f316a60d85fe821282d7a315fe977b5',
 };
